885d344c7aeb5acb4aeafaaadf21c2de6c870516
Stefan Parviainen <pafcu@iki.fi>
2015-11-29 10:22:56 +0200

    Add license headers

--- src/duo_clock.c
+++ src/duo_clock.c
@@ -1,10 +1,24 @@
 /*
-DuoClock
+This file is part of DuoClock.
 
-Original author: Stefan Parviainen, 2015
-Licensed under the GPLv3, see LICENCE
+DuoClock is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
+DuoClock is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with DuoClock.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+/*
+Main file 
+*/
+
 #include <pebble.h>
 #include "duo_clock.h"
 
