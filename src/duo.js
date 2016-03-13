53900902e968380d4ee3238bd555c6e80426dce2
Stefan Parviainen <stefan.parviainen@iki.fi>
2016-03-13 21:03:41 +0200

    Revert "Fix problem with "una", saving settings"
    This reverts commit 57b9da40e950f264b94b5a5d531bbf7aff08f463.

--- src/duo.js
+++ src/duo.js
@@ -99,7 +99,7 @@ function switch_lang(lang, then_do) {
 }
 
 function get_words(username, password, lang) {
-   if(username === null || username == '') {
+   if(username === null) {
     send_words([]);
     return;
   }
